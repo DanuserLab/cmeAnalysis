f663b9de61f711c5775f78f21f6724f98dc97e9e
Danuser CI User <danuser_ci@Nucleus007.cm.cluster>
2020-02-05 13:27:16 -0600

    Danuser Lab CI pipeline build # 75561

--- software/GaussFitND.m
+++ software/GaussFitND.m
@@ -61,20 +61,20 @@ function [parameters,sigmaParameters,Q,chiSquared,degreesOfFreedom,...
 %
 % Copyright (C) 2020, Danuser Lab - UTSouthwestern 
 %
-% This file is part of NewUtrack3DPackage.
+% This file is part of CMEAnalysis_Package.
 % 
-% NewUtrack3DPackage is free software: you can redistribute it and/or modify
+% CMEAnalysis_Package is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
-% NewUtrack3DPackage is distributed in the hope that it will be useful,
+% CMEAnalysis_Package is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 % 
 % You should have received a copy of the GNU General Public License
-% along with NewUtrack3DPackage.  If not, see <http://www.gnu.org/licenses/>.
+% along with CMEAnalysis_Package.  If not, see <http://www.gnu.org/licenses/>.
 % 
 % 
 
