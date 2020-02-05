bb1dd78eaa90f887c33ebf81c550848fb91969e8
Danuser CI User <danuser_ci@Nucleus007.cm.cluster>
2020-02-04 18:17:07 -0600

    Danuser Lab CI pipeline build # 75404

--- software/getLCCBCopyright.m
+++ software/getLCCBCopyright.m
@@ -17,24 +17,24 @@ function copyright = getLCCBCopyright()
 %
 % Copyright (C) 2020, Danuser Lab - UTSouthwestern 
 %
-% This file is part of CMEAnalysis_Package.
+% This file is part of NewUtrack3DPackage.
 % 
-% CMEAnalysis_Package is free software: you can redistribute it and/or modify
+% NewUtrack3DPackage is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
-% CMEAnalysis_Package is distributed in the hope that it will be useful,
+% NewUtrack3DPackage is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 % 
 % You should have received a copy of the GNU General Public License
-% along with CMEAnalysis_Package.  If not, see <http://www.gnu.org/licenses/>.
+% along with NewUtrack3DPackage.  If not, see <http://www.gnu.org/licenses/>.
 % 
 % 
 
 % Set year and version information
 str_year = datestr(date,'YYYY');
 copyright = sprintf('Copyright %s Danuser Lab - UTSouthwestern', str_year);
-% -- TEST CI pipeline deploy to GITHUB -- CI pipeline build # 75403
+% -- TEST CI pipeline deploy to GITHUB -- CI pipeline build # 75404
--- software/linspaceNDim.m
+++ software/linspaceNDim.m
@@ -28,20 +28,20 @@ function y = linspaceNDim(d1, d2, n)
 %
 % Copyright (C) 2020, Danuser Lab - UTSouthwestern 
 %
-% This file is part of CMEAnalysis_Package.
+% This file is part of NewUtrack3DPackage.
 % 
-% CMEAnalysis_Package is free software: you can redistribute it and/or modify
+% NewUtrack3DPackage is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 % 
-% CMEAnalysis_Package is distributed in the hope that it will be useful,
+% NewUtrack3DPackage is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
 % 
 % You should have received a copy of the GNU General Public License
-% along with CMEAnalysis_Package.  If not, see <http://www.gnu.org/licenses/>.
+% along with NewUtrack3DPackage.  If not, see <http://www.gnu.org/licenses/>.
 % 
 % 
 
