01f676787da234149141d0f22838e00939673f53
Danuser CI User <danuser_ci@NucleusB107.cm.cluster>
2026-05-21 00:07:18 -0500

    Danuser Lab CI pipeline build #

---
+++ software/inputImLPackageList.m
@@ -0,0 +1,30 @@
+function ImLpackList = inputImLPackageList()
+    % inputImLPackageList - Returns packages using ImageList as input.
+    %
+    % Add ImageList-native packages here so generic package GUI helper
+    % functions can treat ImageList owners like MovieList owners.
+%
+% Copyright (C) 2026, Danuser Lab - UTSouthwestern 
+%
+% This file is part of CMEAnalysis_Package.
+% 
+% CMEAnalysis_Package is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% CMEAnalysis_Package is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with CMEAnalysis_Package.  If not, see <http://www.gnu.org/licenses/>.
+% 
+% 
+
+    ImLpackList = {
+        'NewFishATLASPackage', ...
+        % Add new packages here
+    };
+end
